bd0ef720484bbc3dd53f565ce9d4cd52ab57a939
jhkang1313 <jhkang1313@gmail.com>
2024-08-20 00:24:47 +0900

    동별 적도 경도 위치 보내기 개발중

--- landlist.py
+++ landlist.py
@@ -2,11 +2,21 @@ import requests as requests
 
 totalRegion = '0000000000'
 region_list_url = 'https://new.land.naver.com/api/regions/list?cortarNo={0}'
-dong_list_url = 'https://new.land.naver.com/api/complexes/single-markers/2.0?cortarNo={0}&zoom=16&priceType=RETAIL&markerId&markerType&selectedComplexNo&selectedComplexBuildingNo&fakeComplexMarker&realEstateType=APT&tradeType=A1&tag=%3A%3A%3A%3A%3A%3A%3A%3A&rentPriceMin=0&rentPriceMax=900000000&priceMin=0&priceMax=120000&areaMin=66&areaMax=115&oldBuildYears=15&recentlyBuildYears&minHouseHoldCount=198&maxHouseHoldCount&showArticle=false&sameAddressGroup=true&minMaintenanceCost&maxMaintenanceCost&directions=&leftLon=126.8884756&rightLon=126.9245244&topLat=37.5420249&bottomLat=37.5273744&isPresale=false'
+dong_list_url = 'https://new.land.naver.com/api/complexes/single-markers/2.0'
 
 latDegree = 0.0073249
 lonDegree = 0.0180244
 default_condition = {
+  'cortarNo': '1156011700',
+  'zoom': 16,
+  'priceType': 'RETAIL',
+  'markerId' : None,
+  'markerType' : None,
+  'selectedComplexNo' : None,
+  'selectedComplexBuildingNo' : None,
+  'fakeComplexMarker' : None,
+  'realEstateType' : 'APT',
+  'tradeType' : 'A1',
   'rentPriceMin': 0,
   'rentPriceMax': 900000000,
   'priceMin': 0,
@@ -29,7 +39,13 @@ default_condition = {
   'isPresale': False
 }
 
+def makeApiUrl(url, param):
+  url = url + "?"
+  for key in param.keys():
+    url += key + "=" + str(param.get(key)) + "&"
+  return url[:-1]
 
+print(makeApiUrl(dong_list_url, default_condition))
 
 header = {
   "Accept" : "*/*",
@@ -59,6 +75,12 @@ for sidoItem in sidoList:
         dongList = response.json()['regionList']
         # 동 탐색
         for dongItem in dongList:
+          newParam = default_condition
+          newParam['cortarNo'] = dongItem.get('cortarNo')
+          newParam['leftLon'] = dongItem.get('centerLon') - lonDegree
+          newParam['rightLon'] = dongItem.get('centerLon') + lonDegree
+          newParam['topLat'] = dongItem.get('centerLat') - latDegree
+          newParam['bottomLat'] = dongItem.get('centerLat') + latDegree
 
           response = requests.get(dong_list_url.format(dongItem.get('cortarNo')), headers=header)
           print(f"{dongItem.get('cortarName')} : {len(response.json())}")
